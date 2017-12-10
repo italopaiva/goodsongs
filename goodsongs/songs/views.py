0e963d84e72ac574879ea4cbca7b516767b6cbc1
Italo Paiva <italosensei@hotmail.com>
2017-12-10 09:35:46 -0200

    Creating endpoint to list all songs

--- goodsongs/songs/views.py
+++ goodsongs/songs/views.py
@@ -1,6 +1,16 @@
 """Songs endpoints."""
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 songs = Blueprint('songs', __name__)
+
+
+@songs.route('', methods=['GET'])
+def get_songs():
+    """
+    Return a list of songs with some details on them.
+
+    Accepts a page parameter as a query string to paginate the results.
+    """
+    return jsonify([{'id': 1, 'name': 'song name'}])
