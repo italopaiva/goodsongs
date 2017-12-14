5821a73403710278cd1a3e6ac3792d63f52dbb17
Italo Paiva <italosensei@hotmail.com>
2017-12-14 00:29:56 -0200

    Creating database config values for development and testing environments

---
+++ config/development.py
@@ -0,0 +1,10 @@
+"""Development configuration values."""
+
+DEGUG = True
+
+DATABASE_NAME = 'goodsongs'
+
+# Database config
+MONGODB_DB = DATABASE_NAME
+MONGODB_HOST = 'mongo'
+MONGODB_PORT = 27017
---
+++ config/test.py
@@ -0,0 +1,10 @@
+"""Testing configuration values."""
+
+DEGUG = True
+
+DATABASE_NAME = 'goodsongs_test'
+
+# Database config
+MONGODB_DB = DATABASE_NAME
+MONGODB_HOST = 'mongo'
+MONGODB_PORT = 27017
