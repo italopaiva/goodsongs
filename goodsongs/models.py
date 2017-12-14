bc12b793badd3f7bdf3c2c71fae0478c4080fa58
Italo Paiva <italosensei@hotmail.com>
2017-12-14 00:23:50 -0200

    Creating a method to parse a songs JSON file into the database

--- goodsongs/models.py
+++ goodsongs/models.py
@@ -1,5 +1,7 @@
 """App models."""
 
+from datetime import datetime
+
 from flask_mongoengine import MongoEngine
 
 
@@ -17,7 +19,26 @@ class Song(db.Document):
 
     meta = {'collection': 'songs'}
 
+    @classmethod
+    def load_songs_from_file(cls, songs_file):
+        """Load songs from a JSON like file."""
+        import json
+
+        songs = []
+        with open(songs_file, 'r') as song_file:
+            for line in song_file:
+                song = json.loads(line)
+                song_obj = cls(
+                    artist=song['artist'],
+                    title=song['title'],
+                    difficulty=song['difficulty'],
+                    level=song['level'],
+                    released=datetime.strptime(song['released'], '%Y-%m-%d'),
+                )
+                songs.append(song_obj)
+        cls.objects.insert(songs)
+
 
 def seed_database(songs='', **_kwargs):
     """Seed the database with the given files."""
-    pass
+    Song.load_songs_from_file(songs)
