e82d3058ca7d6bce96cda4aca7270a941b502b6a
Italo Paiva <italosensei@hotmail.com>
2017-12-14 00:22:41 -0200

    Creating Song model

--- goodsongs/models.py
+++ goodsongs/models.py
@@ -6,6 +6,18 @@ from flask_mongoengine import MongoEngine
 db = MongoEngine()
 
 
+class Song(db.Document):
+    """Model to describe a Song."""
+
+    artist = db.StringField()
+    title = db.StringField()
+    difficulty = db.FloatField()
+    level = db.IntField()
+    released = db.DateTimeField()
+
+    meta = {'collection': 'songs'}
+
+
 def seed_database(songs='', **_kwargs):
     """Seed the database with the given files."""
     pass
