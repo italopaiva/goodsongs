adc31ffd539fcf1f9e3cd4bde3dcb016f7c55b2b
Italo Paiva <italosensei@hotmail.com>
2017-12-14 00:26:00 -0200

    Creating Song factory for tests

---
+++ tests/factories/song_factory.py
@@ -0,0 +1,19 @@
+import datetime
+
+from factory import Sequence
+from factory.mongoengine import MongoEngineFactory
+
+from goodsongs.models import Song
+
+
+class SongFactory(MongoEngineFactory):
+    class Meta:
+        model = Song
+
+    artist = Sequence(lambda n: 'Artist %d' % n)
+    title = Sequence(lambda n: 'Title %d' % n)
+    difficulty = Sequence(lambda n: float(n))
+    level = Sequence(lambda n: n)
+    released = Sequence(
+        lambda n: datetime.datetime.now() + datetime.timedelta(days=n)
+    )
