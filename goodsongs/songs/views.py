616fec68d8add7400fcd79608939e3cef63fe5f3
Italo Paiva <italosensei@hotmail.com>
2017-12-14 00:52:03 -0200

    Returning some hardcoded response prototype to pass the test

--- goodsongs/songs/views.py
+++ goodsongs/songs/views.py
@@ -11,6 +11,27 @@ def get_songs():
     """
     Return a list of songs with some details on them.
 
-    Accepts a page parameter as a query string to paginate the results.
+    Accepts page and per_page parameters
+    as a query strings to paginate the results.
     """
-    return jsonify([{'id': 1, 'name': 'song name'}])
+    return jsonify({
+        'data': [
+            {
+                '_id': {
+                    '$oid': '5a31d485bdb63d001da756d7'
+                },
+                'artist': 'The Yousicians',
+                'difficulty': 14.6,
+                'level': 13,
+                'released': {
+                    '$date': 1477440000000
+                },
+                'title': 'Lycanthropic Metamorphosis'
+            }
+        ],
+        'meta': {
+            'current_page': 1,
+            'next_page': 2,
+            'total_pages': 3
+        }
+    })
