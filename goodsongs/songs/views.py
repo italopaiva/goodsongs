ea59fd5555e80752de58a50e7e2200ab9987075e
Italo Paiva <italosensei@hotmail.com>
2017-12-14 01:02:42 -0200

    Retrieving all songs and paginating them

--- goodsongs/songs/views.py
+++ goodsongs/songs/views.py
@@ -1,6 +1,9 @@
 """Songs endpoints."""
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from goodsongs.models import Song
+from goodsongs.pagination import get_pagination_params, paginate
 
 
 songs = Blueprint('songs', __name__)
@@ -14,24 +17,8 @@ def get_songs():
     Accepts page and per_page parameters
     as a query strings to paginate the results.
     """
-    return jsonify({
-        'data': [
-            {
-                '_id': {
-                    '$oid': '5a31d485bdb63d001da756d7'
-                },
-                'artist': 'The Yousicians',
-                'difficulty': 14.6,
-                'level': 13,
-                'released': {
-                    '$date': 1477440000000
-                },
-                'title': 'Lycanthropic Metamorphosis'
-            }
-        ],
-        'meta': {
-            'current_page': 1,
-            'next_page': 2,
-            'total_pages': 3
-        }
-    })
+    page, per_page = get_pagination_params(request)
+    all_songs = Song.objects
+    paginated_songs = paginate(all_songs, page=page, per_page=per_page)
+
+    return jsonify(paginated_songs)
